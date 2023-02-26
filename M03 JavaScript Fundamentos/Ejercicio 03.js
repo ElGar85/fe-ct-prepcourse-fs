7066816b99c8d00e9696af545bb24ba43fb63873
ElGar85 <eligu4@gmail.com>
2023-02-26 11:34:41 -0500

    HomeWork 03 terminado

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -2,39 +2,74 @@
 /*3️⃣ EJERCICIO 03 3️⃣*/
 
 function sonIguales(x, y) {
-   // Retorna true si "x" e "y" son iguales.
-   // De lo contrario, retorna false.
+   // Retorna True si "x" e "y" son iguales.
+   // De lo contrario, retorna False.
    // Tu código:
+   var x = x;
+   var y = y;
+   if(x==y){
+      return true;
+   }else{
+      return false;
+   }
+
 }
 
 function tienenMismaLongitud(str1, str2) {
-   // Retorna true si los dos strings tienen la misma longitud.
-   // De lo contrario, retorna false.
+   // Retorna True si los dos strings tienen la misma longitud.
+   // De lo contrario, retorna False.
    // Tu código:
+   var str1 = str1;
+   var str2 = str2;
+   if (str1.length == str2.length){
+      return true;
+   }else{
+      return false;
+   }
 }
 
 function menosQueNoventa(num) {
-   // Retorna true si el argumento "num" es menor que noventa.
-   // De lo contrario, retorna false.
+   // Retorna True si el argumento "num" es menor que noventa.
+   // De lo contrario, retorna False.
    // Tu código:
+   var num = num;
+   if (num < 90){
+      return true;
+   }
+   return false;
 }
 
 function mayorQueCincuenta(num) {
-   // Retorna true si el argumento "num" es mayor que cincuenta.
-   // De lo contrario, retorna false.
+   // Retorna True si el argumento "num" es mayor que cincuenta.
+   // De lo contrario, retorna False.
    // Tu código:
+   var num = num;
+   if (num > 50){
+      return true;
+   }
+   return false;
 }
 
 function esPar(num) {
-   // Retorna true si "num" es par.
-   // De lo contrario, retorna false.
+   // Retorna True si "num" es par.
+   // De lo contrario, retorna False.
    // Tu código:
+   var num = num;
+   if (num % 2 == 0){
+      return true;
+   }
+   return false;
 }
 
 function esImpar(num) {
-   // Retorna true si "num" es impar.
-   // De lo contrario, retorna false.
+   // Retorna True si "num" es impar.
+   // De lo contrario, retorna False.
    // Tu código:
+   var num = num;
+   if (num % 2 == 1){
+      return true;
+   }
+   return false;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
